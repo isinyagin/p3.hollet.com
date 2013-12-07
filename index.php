58785cadf6340f2f71d459daacdfe7bf50b33c9b
Ivan Sinyagin <isinyagin@fas.harvard.edu>
2013-12-06 22:07:39 -0500

    Dragabble version, mostly working

--- index.php
+++ index.php
@@ -2,32 +2,38 @@
 <html lang='en'>
 
 <head>
-    <title>CS15 Project 1 -- Info</title>
+    <title>CS15 Project 3 </title>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <link href='./css/bootstrap.css' rel='stylesheet' media='screen'>
+    <link href='./css/base.css' rel='stylesheet' media='screen'>
+    <link href='./css/todo.css' rel='stylesheet' media='screen'>
 </head>
 
 <body>
     <section class='container'>
         <div class='row'>
-            <section class='col-md-4'>
-                <h1>Simple to-do list</h1>
+            <div class='col-md-4 icontainer'>
+                <h1>Simplist</h1>
 
-                <ul id='list' class='list-group' contenteditable>
-                    <li class='list-group-item'>Enter an item</li>
-                </ul>
-
-                <p>
-                    <button class='btn' id ='save'>Save</button>
+                <form role='form' name='todo' method='post' action=''>
+                    <div class='form-group'>
+                        <input class='form-control' type='text' name='todoItem' id='todoItem' autofocus>
+                    </div>
+                    <button class='btn' type='submit' name='addItem' id='addItem'>Add Item</button>
                     <button class='btn' id ='clear'>Clear</button>
-                    <div id='report'></div>
+                </form>
+
+                <ul id='list' class='list-group sortable'></ul>
+
+                <p id='clear'>
                 </p>
-            </section>
+            </div>
         </div>
     </section><!-- container -->
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script>
+    <script src='./js/jquery.sortable.js'></script>
     <script src='./js/bootstrap.js'></script>
     <script src='./js/todo.js'></script>
 </body>
--- js/todo.js
+++ js/todo.js
@@ -1,17 +1,95 @@
 $(document).ready(function() {
+	var newList = true;
 
-    $('#save').click(function(e) {
+	$('#addItem').on('click', function(e) {
+		e.preventDefault();
+        var newListItem;
+		if (newList == true) {
+			var theValue = $('#todoItem').val();
+			newListItem = '<li><span class="handle"> :: </span> <input class="listItem" value="' + 
+                            theValue + 
+                            '"><a class="removeListItem" style="display: none;" href="#"> X </a> </li>';
+			newList = false;	
+		} else {
+			var theValue = $("#todoItem").val();
+		    newListItem = $('#list li:last').clone();
+			newListItem.find('input').attr('value', theValue); 
+		}
+
+		var theCount = $("#list li").length + 1;
+		if (theCount > 1)
+            $('#clear').css('display','block');
+
+		$('#list').append(newListItem);
+
+
+		$('#todoItem').val('');
+		$('#todoItem').focus();
+		$('.sortable').sortable('destroy');
+		$('.sortable').sortable({
+			handle: '.handle'
+		});
+        
         localStorage.setItem('todoList', $('#list').html());
-        $('#report').html("You items have been saved");
+	});
+		
+    $('input[type="text"]').keydown(function(e) {
+        var key = e.charCode ? e.charCode : (e.keyCode ? e.keyCode : 0);
+        if (key === 13) {
+            e.preventDefault();
+            var inputs = $(this).closest('form').find(':input:visible');
+            inputs.eq(inputs.index(this) + 1).focus();
+        }
+    });
+
+    $('#list').change('.listItem', function(e) {
+        var current = $(this).val();
+        $(this).attr('value', current);
+        localStorage.setItem('todoList', $('#list').html());
+    });
+
+    $('.sortable').sortable().bind('sortupdate', function() {
+        localStorage.setItem('todoList', $('#list').html());
+    });
+    
+    $('#list').mouseover('li', function() {
+        $(this).find('a').css('display', 'block');
+    });
+    
+    $('#list').mouseout('li', function() {
+        $(this).find('a').css('display', 'none');
+    });
+
+    $('#list').click('.removeListItem', function(e) {
+        e.preventDefault();
+        $(this).parent().remove();
+        localStorage.setItem('todoList', $('#list').html());
     });
 
     $('#clear').click(function(e) {
+        e.preventDefault();
+        $('#list').children().remove();
+        newList = true;
+        $('#dotoItem').val('');
+        $('#clear').css('display','none');
+        $('#todoItem').focus();
         localStorage.removeItem('todoList');
-        location.reload();
     });
 
-    (function() {
-        if (localStorage.getItem('todoList'))
+    function load() {
+        if (localStorage.getItem('todoList')) {
             $('#list').html(localStorage.getItem('todoList'));
-    })();
-});
+            $('.sortable').sortable('destroy');
+            $('.sortable').sortable({
+                handle: '.handle'
+            });
+
+            var theCount = $("#list li").length + 1;
+            if (theCount > 1)
+                $('#clear').css('display','block');
+        }
+    };
+
+    load();    
+    
+}); /* end of document.ready */
